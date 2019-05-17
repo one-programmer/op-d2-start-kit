526be8b6d53edb5635c64e8c9ea906f62c81e843
zhaoying <zhaoying@1programmer.com>
2019-05-17 13:39:07 +0800

    比赛详情

--- src/pages/matchManage/details.vue
+++ src/pages/matchManage/details.vue
@@ -1,20 +1,33 @@
 <template>
   <d2-container>
     <el-card>
-      <iframe src="https://basketball-m.1programmer.com/game_details?gameId=16&token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6NSwidWlkIjoid2FIQ2NyM2IiLCJoZWFkX2ltZyI6Imh0dHBzOi8vd3gucWxvZ28uY24vbW1vcGVuL3ZpXzMyL1EwajRUd0dUZlRKVWxSZDRJS0syR01sQ01zWmlicmV1Y0VkdXhpY1JsMHpKejhwb2MzN2dLbWtUNzNZbnk1RWxxRUVtRG9abWtnamlic0VpYjFMMVcyc1Bxdy8xMzIiLCJleHAiOjE1NjA0ODA1MzcsImdlbmRlciI6Miwibmlja25hbWUiOiJNYXkifQ.-NHzXt_4s1tT9Hs-6PB8WjS4sLnJHzCGO0MF-_GN46Y"></iframe>
+      <iframe :src="src"></iframe>
     </el-card>
   </d2-container>
 </template>
 
 <script>
+import db from '../../plugin/db'
+import config from '../../config'
 export default {
+  data () {
+    return {
+      id: this.$route.params.id,
+      src: ''
+    }
+  },
 
+  created () {
+    const token = db.get('token').value()
+    this.src = `${config.WEB_URL}/game_details?gameId=${this.id}&token=${token}`
+  }
 }
 </script>
 
 <style lang="scss" scoped>
 iframe {
-  width: 350px;
-  height: 700px;
+  border: none;
+  width: 375px;
+  height: 667px;
 }
 </style>
